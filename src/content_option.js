a8388dea0bc4f4018a7fac5307058ba01d8c4065
azzaaass <mobilelegendsazza@gmail.com>
2025-01-01 15:23:15 +0700

    first init

---
+++ src/content_option.js
@@ -0,0 +1,131 @@
+const logotext = "Az";
+const meta = {
+  title: "Baihaqi Ilmi",
+  description:
+    "I am a self-taught student at Telkom University Surabaya with a strong passion for design and programming",
+};
+
+const introdata = {
+  title: "I’m Baihaqi Ilmi",
+  animated: {
+    first: "Web Developer",
+    second: "UI/UX Designer",
+    third: "Freelancer",
+  },
+  description: "Self-taught student at Telkom University Surabaya with a strong passion for design and programming",
+  your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+  title: "about my self",
+  aboutme:
+    "I am a self-taught student at Telkom University Surabaya with a strong passion for design and programming. I frequently work on projects utilizing Figma, Tailwind, Java, Laravel, React, and Flutter. I am dedicated to continuously learning new skills and committed to delivering high-quality results.",
+};
+const worktimeline = [
+  {
+    jobtitle: "Backend Developer",
+    where: "SPM Tel-U Surabaya",
+    date: "09/2024 - 01/2025",
+  },
+  {
+    jobtitle: "Frontend Developer",
+    where: "KJPP Gear Surabaya",
+    date: "05/2023 - 11/2024",
+  },
+  {
+    jobtitle: "Lab Assistent",
+    where: "Telkom University Surabaya",
+    date: "09/2021 - 01/2025",
+  },
+  {
+    jobtitle: "IT Support & Designer",
+    where: "Nastya Global Corp",
+    date: "05/2019 - 02/2020",
+  },
+];
+
+const skills = [
+  {
+    name: "Laravel, React, Flutter, Tailwind, Figma & IoT",
+  },
+];
+
+const services = [
+  {
+    title: "Sosial Project Innovillage (2023)",
+    description:
+      "Received funding for a social project in Inovillage titled SEAWID: Seaweed Farm Management.",
+  },
+  {
+    title: "Dicoding Course",
+    description:
+      "Finished course in Dicoding, including SQL, Javascript, Dart and Flutter.",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description: "Simda (SPM Tel-U Surabaya)",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/800/?grayscale",
+    description: "Dagri Online",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description: "SSC Website",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description: "Smart Loker",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description: "Puskesmas Cisadea",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description: "Tusmart",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description: "Green School",
+    link: "https://www.figma.com/design/Azc5YxpeViDMB03X7TYK6q/Green-School?node-id=0-1&t=tk6ZCbf2yOkKBUui-1",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "baihaqiilmi42@gmail.com",
+  YOUR_FONE: "(+62) 831-9277-7446",
+  description: "Bersedia untuk berpartisipasi dalam project anda",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+  github: "https://github.com/azzaaass",
+  instagram: "https://www.instagram.com/azzaaass",
+  linkedin: "https://www.linkedin.com/in/baihaqi-ilmi",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
